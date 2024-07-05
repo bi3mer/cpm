8118ff395d67ebf1aed7647a658c387aec4e879d
bi3mer <colan@comcast.net>
2024-07-04 21:53:58 -0400

    get password implemented

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -22,6 +23,36 @@ void list() {
   fclose(file);
 }
 
+void get(const char *key) {
+  FILE *file = fopen(FILE_PATH, "r");
+  if (file == NULL) {
+    printf("%s not found\n", key);
+    return;
+  }
+
+  char buffer[BUFFER_SIZE];
+  char *token;
+
+  bool found = false;
+  while (fgets(buffer, BUFFER_SIZE, file)) {
+    token = strtok(buffer, ",");
+    if (strcmp(token, key) == 0) {
+      token = strtok(NULL, ",");
+      token[strlen(token) - 1] = '\0';
+      printf("-- %s\n", token);
+
+      found = true;
+      break;
+    }
+  }
+
+  fclose(file);
+
+  if (!found) {
+    printf("%s not found\n", key);
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     printf("Please include an argument (--list, --add, --get)\n");
@@ -31,6 +62,8 @@ int main(int argc, char *argv[]) {
   const char *cmd = argv[1];
   if (strcmp(cmd, "--list") == 0) {
     list();
+  } else if (strcmp(cmd, "--get") == 0) {
+    get(argv[2]);
   } else {
     printf("Unsupported argument: %s\n", cmd);
     return 1;
