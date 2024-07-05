c37a0c33d35d43e0aeb3410e7ae520ffa2d83fcd
bi3mer <colan@comcast.net>
2024-07-04 21:44:38 -0400

    list functionality done

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-  printf("hello, world!\n");
-  return 0;
+#define FILE_PATH "pass.csv"
+#define BUFFER_SIZE 256
+
+void list() {
+  FILE *file = fopen(FILE_PATH, "r");
+  if (file == NULL) {
+    printf("No passwords stored...\n");
+    return;
+  }
+
+  char buffer[BUFFER_SIZE];
+  char *token;
+
+  while (fgets(buffer, BUFFER_SIZE, file)) {
+    token = strtok(buffer, ",");
+    printf("-- %s\n", token);
+  }
+
+  fclose(file);
 }
 
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("Please include an argument (--list, --add, --get)\n");
+    return 1;
+  }
+
+  const char *cmd = argv[1];
+  if (strcmp(cmd, "--list") == 0) {
+    list();
+  } else {
+    printf("Unsupported argument: %s\n", cmd);
+    return 1;
+  }
+
+  return 0;
+}
