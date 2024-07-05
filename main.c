cfe238cc5e46b13b4a4ae1c9447f0a9d2941c8d2
bi3mer <colan@comcast.net>
2024-07-05 10:11:26 -0400

    basic functionality is done, now need to add encryption and decryption

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ void get(const char *file_path, const char *key) {
     if (strcmp(token, key) == 0) {
       token = strtok(NULL, ",");
       token[strlen(token) - 1] = '\0';
-      printf("-- %s\n", token);
+      printf("pass: %s\n", token);
 
       found = true;
       break;
@@ -54,14 +54,53 @@ void get(const char *file_path, const char *key) {
   }
 }
 
+void add(const char *file_path, const char *key, const char *pass) {
+  FILE *file = fopen(file_path, "r");
+  if (file == NULL) {
+    file = fopen(file_path, "w");
+    fprintf(file, "%s,%s\n", key, pass);
+    fclose(file);
+    printf("Stored.\n");
+    exit(1);
+  }
+
+  FILE *temp = fopen("temp", "w");
+  fprintf(temp, "%s,%s\n", key, pass); // store new pass
+
+  char buffer[BUFFER_SIZE];
+  char buffer2[BUFFER_SIZE];
+  char *token;
+
+  bool found = false;
+  while (fgets(buffer, BUFFER_SIZE, file)) {
+    // only write lines that don't use the same key, so this is basically
+    // supporting overwritting
+    strcpy(buffer2, buffer);
+    const char *token = strtok(buffer, ",");
+    if (strcmp(token, key) != 0) {
+      fprintf(temp, "%s", buffer2);
+    }
+  }
+
+  fclose(temp);
+  fclose(file);
+
+  remove(file_path);
+  rename("temp", file_path);
+
+  printf("Stored.\n");
+}
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    fprintf(stderr, "Please include an argument (--list, --add, --get)\n");
+    printf("Please include an argument:\n");
+    printf("  --list\n");
+    printf("  --get {key}\n");
+    printf("  --add {key} {pass}\n");
     exit(1);
   }
 
   const char *file_path = getenv("cmppath");
-  printf("%s\n", file_path);
   if (!file_path) {
     fprintf(stderr, "Please set an enviornment variable `cmp_path` for where "
                     "you want passwords stored.\n");
@@ -77,10 +116,12 @@ int main(int argc, char *argv[]) {
   const char *cmd = argv[1];
   if (strcmp(cmd, "--list") == 0) {
     list(file_path);
-  } else if (strcmp(cmd, "--get") == 0) {
+  } else if (argc == 3 && strcmp(cmd, "--get") == 0) {
     get(file_path, argv[2]);
+  } else if (argc == 4 && strcmp(cmd, "--add") == 0) {
+    add(file_path, argv[2], argv[3]);
   } else {
-    fprintf(stderr, "Unsupported argument: %s\n", cmd);
+    fprintf(stderr, "Unsupported command: %s\n", cmd);
     return 1;
   }
 
